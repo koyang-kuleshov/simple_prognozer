bbe4d0d214785b3da14fd7070e9a00e25fff9bb7
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-06-29 19:50:08 +0300

    Новая модель данных. Миграции.

--- simple_prognozer/mainapp/models.py
+++ simple_prognozer/mainapp/models.py
@@ -1,34 +1,50 @@
 from django.db import models
 
 
-class MainTable(models.Model):
+class Country(models.Model):
+    country_region = models.CharField(verbose_name='Country',
+                                      max_length=128,
+                                      primary_key=True
+                                      )
+
+
+class Subdivision(models.Model):
+    country = models.ForeignKey(Country,
+                                on_delete=models.CASCADE,
+                                )
+    subdivision = models.CharField(verbose_name='Province, Region or City',
+                                   max_length=128,
+                                   default=0
+                                   )
     # FIPS только для US
-    fips = models.PositiveIntegerField(verbose_name='FIPS',
+    fips = models.PositiveIntegerField(verbose_name='FIPS US inner number',
                                        blank=True
                                        )
     # Admin2 только для US
-    admin2 = models.CharField(verbose_name='Admin2',
+    admin2 = models.CharField(verbose_name='Admin2 County name. US only',
                               max_length=128
                               )
-    province_state = models.CharField(verbose_name='Province',
-                                      max_length=128
-                                      )
-    country_region = models.CharField(verbose_name='Country_region',
-                                      max_length=128
-                                      )
-    last_update = models.CharField(verbose_name='Last_update',
+    last_update = models.CharField(verbose_name='Last update',
                                    max_length=128
                                    )
     # Широта
     lat = models.DecimalField(verbose_name='Latitude',
                               max_digits=19,
                               decimal_places=16,
-                              default=0
+                              default=0.0
                               )
     # Долгота
     longitude = models.DecimalField(verbose_name='Longitude',
                                     max_digits=19,
                                     decimal_places=16,
+                                    default=0.0
+                                    )
+
+
+class MainTable(models.Model):
+    subdivision = models.ForeignKey(Subdivision,
+                                    on_delete=models.CASCADE,
+                                    related_name='maintable',
                                     default=0
                                     )
     # Заболевшие
@@ -47,26 +63,20 @@ class MainTable(models.Model):
     active = models.PositiveIntegerField(verbose_name='Active',
                                          default=0
                                          )
-    # Формат Khakassia, Republic Russia
-    combined_key = models.CharField(verbose_name='Combined_key',
-                                    max_length=128,
-                                    default='Russia',
-                                    unique=True
-                                    )
     # Заболеваемость
-    incidence_rate = models.DecimalField(verbose_name='Incidence_rate',
+    incidence_rate = models.DecimalField(verbose_name='Incidence rate',
                                          max_digits=19,
                                          decimal_places=16,
                                          blank=True
                                          )
     # Соотношение Заболеваемости И Летальности в % на 100 000 человек
     case_fatality_ratio = models.\
-        DecimalField(verbose_name='Case_fatality_ratio',
+        DecimalField(verbose_name='Case fatality ratio',
                      max_digits=19,
                      decimal_places=16,
                      default=0
                      )
-    testing_rate = models.DecimalField(verbose_name='Testin_rate',
+    testing_rate = models.DecimalField(verbose_name='Testing rate',
                                        max_digits=19,
                                        decimal_places=16,
                                        default=0
@@ -81,7 +91,7 @@ class MainTable(models.Model):
         PositiveIntegerField(verbose_name='Region population',
                              default=0
                              )
-    # Округляем в большую сторону
+    # 80% от region_population округляем в большую сторону
     region_limit = models.PositiveIntegerField(verbose_name='Region limit',
                                                default=0
                                                )
