66b34de6341d510effde8b2ed535bbb1bc893caf
Iakubov <max.iakubov@gmail.com>
2020-06-30 15:01:35 +0300

    models fix

--- simple_prognozer/mainapp/models.py
+++ simple_prognozer/mainapp/models.py
@@ -2,67 +2,69 @@ from django.db import models
 
 
 class Country(models.Model):
-    country_region = models.CharField(verbose_name='Country',
-                                      max_length=128,
-                                      primary_key=True
-                                      )
+    country = models.CharField(verbose_name='Country',
+                               max_length=128,
+                               unique=True
+                               )
 
 
 class Subdivision(models.Model):
     country = models.ForeignKey(Country,
-                                on_delete=models.CASCADE,
+                                on_delete=models.DO_NOTHING,
                                 )
     subdivision = models.CharField(verbose_name='Province, Region or City',
                                    max_length=128,
-                                   default=0
                                    )
     # FIPS только для US
-    fips = models.PositiveIntegerField(verbose_name='FIPS US inner number',
-                                       blank=True
-                                       )
+    fips = models.IntegerField(verbose_name='FIPS US inner number',
+                               null=True
+                               )
     # Admin2 только для US
     admin2 = models.CharField(verbose_name='Admin2 County name. US only',
-                              max_length=128
+                              max_length=128,
+                              null=True,
                               )
-    last_update = models.CharField(verbose_name='Last update',
-                                   max_length=128
-                                   )
     # Широта
     lat = models.DecimalField(verbose_name='Latitude',
                               max_digits=19,
                               decimal_places=16,
-                              default=0.0
+                              null=True
                               )
     # Долгота
     longitude = models.DecimalField(verbose_name='Longitude',
                                     max_digits=19,
                                     decimal_places=16,
-                                    default=0.0
+                                    null=True
                                     )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['admin2', 'country', 'fips'], name='uq_subdivison')
+        ]
 
 
 class MainTable(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.DO_NOTHING)
     subdivision = models.ForeignKey(Subdivision,
-                                    on_delete=models.CASCADE,
-                                    related_name='maintable',
-                                    default=0
+                                    on_delete=models.DO_NOTHING,
+                                    null=True
                                     )
     # Заболевшие
     confirmed = models.PositiveIntegerField(verbose_name='Confirmed',
-                                            default=0
                                             )
     # Смерти
     deaths = models.PositiveIntegerField(verbose_name='Deaths',
-                                         default=0
                                          )
     # Выздоровления
     recovered = models.PositiveIntegerField(verbose_name='Recovered',
-                                            default=0
                                             )
     # Активные больные
     active = models.PositiveIntegerField(verbose_name='Active',
-                                         default=0
                                          )
+
+    last_update = models.DateTimeField(verbose_name='Last update',
+                                       )
+
     # Заболеваемость
     incidence_rate = models.DecimalField(verbose_name='Incidence rate',
                                          max_digits=19,
@@ -70,7 +72,7 @@ class MainTable(models.Model):
                                          blank=True
                                          )
     # Соотношение Заболеваемости И Летальности в % на 100 000 человек
-    case_fatality_ratio = models.\
+    case_fatality_ratio = models. \
         DecimalField(verbose_name='Case fatality ratio',
                      max_digits=19,
                      decimal_places=16,
@@ -81,13 +83,13 @@ class MainTable(models.Model):
                                        decimal_places=16,
                                        default=0
                                        )
-    hospitalization_rate = models.\
+    hospitalization_rate = models. \
         DecimalField(verbose_name='Hospitalization_rate',
                      max_digits=19,
                      decimal_places=16,
                      default=0
                      )
-    region_population = models.\
+    region_population = models. \
         PositiveIntegerField(verbose_name='Region population',
                              default=0
                              )
@@ -98,7 +100,8 @@ class MainTable(models.Model):
 
 
 class TimeSeries(models.Model):
-    region = models.ForeignKey(MainTable, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.DO_NOTHING)
+    subdivision = models.ForeignKey(Subdivision, on_delete=models.DO_NOTHING, null=True)
     last_update = models.DateTimeField()
     # Заболевшие
     confirmed = models.PositiveIntegerField(verbose_name='Confirmed',
