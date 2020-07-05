5fc6096b0f37bf462745ba0ba797213fb4838213
sab1e <antony.sobolev@gmail.com>
2020-07-05 21:50:10 +0500

    added download and convert covid data

--- simple_prognozer/services/data_visualization.py
+++ simple_prognozer/services/data_visualization.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -13,17 +14,17 @@ django.setup()
 from mainapp.models import TimeSeries
 
 
-def graph_confirm(df_by_date):
+def graph_confirm(df_by_date, path):
     fig, ax = plt.subplots()
     ax.plot(df_by_date["last_update"],
              df_by_date["confirmed"]/1000)
 
     plt.xlabel('Дата')
     plt.ylabel('Число подтвержденных случаев, тыс. чел.')
-    plt.savefig('confirmed')
+    plt.savefig(os.path.join(path, 'confirmed'))
 
 
-def graph_death_recovered(df_by_date, labels):
+def graph_death_recovered(df_by_date, labels, path):
     fig, ax = plt.subplots()
 
     for label in labels:
@@ -34,20 +35,20 @@ def graph_death_recovered(df_by_date, labels):
     plt.ylabel('Число случаев, тыс. чел.')
     plt.xlabel('Дата')
     plt.legend()
-    plt.savefig('death_and_recovered')
+    plt.savefig(os.path.join(path, 'death_and_recovered'))
 
 
-def graph_fatality_rate(df_by_date):
+def graph_fatality_rate(df_by_date, path):
     fig, ax = plt.subplots()
     ax.plot(df_by_date["last_update"],
              df_by_date['deaths'] / df_by_date['confirmed'] * 100)
 
     plt.ylabel('Доля умерших среди зараженных, %')
     plt.xlabel('Дата')
-    plt.savefig('fatality_rate')
+    plt.savefig(os.path.join(path, 'fatality_rate'))
 
 
-def graph_fatality_recovered_rate(df_by_date, labels):
+def graph_fatality_recovered_rate(df_by_date, labels, path):
     fig, ax = plt.subplots()
     for label in labels:
         ax.plot(df_by_date['last_update'],
@@ -56,18 +57,20 @@ def graph_fatality_recovered_rate(df_by_date, labels):
     plt.ylabel('Доли умерших и выздоровевших зараженных, %')
     plt.xlabel('Дата')
     plt.legend()
-    plt.savefig('fatality_recovered_rate')
+    plt.savefig(os.path.join(path, 'fatality_recovered_rate'))
 
 
 qs = TimeSeries.objects.all()
 df = read_frame(qs)
 df_by_date = df.groupby(['last_update']).sum().reset_index(drop=None)
+path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'media', 'graph')
 
 mpl.rcParams['figure.figsize'] = [12.0, 5.0]
-mpl.rcParams['figure.dpi'] = 100
+mpl.rcParams['figure.dpi'] = 200
 labels = {"deaths": "Умерли", "recovered": "Выздоровели"}
 
-graph_confirm(df_by_date)
-graph_death_recovered(df_by_date, labels)
-graph_fatality_rate(df_by_date)
-graph_fatality_recovered_rate(df_by_date, labels)
+
+graph_confirm(df_by_date, path)
+graph_death_recovered(df_by_date, labels, path)
+graph_fatality_rate(df_by_date, path)
+graph_fatality_recovered_rate(df_by_date, labels, path)
