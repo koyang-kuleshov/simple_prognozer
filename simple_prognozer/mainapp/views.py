d42a4f4dc388de5bd5f38b358320c82ba3472d57
alyonkazz <alyonkazz@gmail.com>
2020-07-16 14:56:38 +0300

    base template:   add footer

--- simple_prognozer/mainapp/views.py
+++ simple_prognozer/mainapp/views.py
@@ -1,5 +1,6 @@
 import csv
 
+from django.views.generic import ListView
 
 from services import parse
 
@@ -26,10 +27,13 @@ def index(request):
         data_deaths.append(day['deaths__sum'] / 1000)
         data_recovered.append(day['recovered__sum'] / 1000)
 
+    countries = Country.objects.all()
+
     context = {
         'labels': labels,
         'data_confirmed': data_confirmed,
         'data_deaths': data_deaths,
         'data_recovered': data_recovered,
+        'countries': countries,
     }
     return render(request, 'mainapp/index.html', context)
