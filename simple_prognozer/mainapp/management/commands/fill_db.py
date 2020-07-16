92714a87c7cf0565ae9fde15950c4f9f167a0753
Koyang-Kukeshov Michel <michelkoyangkuleshov@gmail.com>
2020-07-16 21:15:57 +0300

    Need to fix fill_db

--- simple_prognozer/mainapp/management/commands/fill_db.py
+++ simple_prognozer/mainapp/management/commands/fill_db.py
@@ -8,14 +8,17 @@ import requests
 from github.MainClass import Github
 import pandas as pd
 
-TOKEN = 'a6c034b6c7677dbe8693ae0ad5bce62ebcfaee18'  # GitHub token
+from simple_prognozer.secret_keys import TOKEN
+
 
 REPO_PATH = 'CSSEGISandData/COVID-19'
 GIT = Github(TOKEN)
 REPO = GIT.get_repo(REPO_PATH)
 DR_REPO_FILE_LIST = 'csse_covid_19_data/csse_covid_19_daily_reports'
-DAILY_REPORTS_DIR_PATH = 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data' \
-                         '/csse_covid_19_daily_reports/'
+DAILY_REPORTS_DIR_PATH = ('https://github.com/CSSEGISandData/COVID-19/raw/'
+                          'master/csse_covid_19_data'
+                          '/csse_covid_19_daily_reports/'
+                          )
 
 
 def get_csv(file_name):
@@ -24,8 +27,9 @@ def get_csv(file_name):
     daily_reports_file_list = REPO.get_contents(DR_REPO_FILE_LIST)
 
     if file_name == 'daily_reports':
-        daily_reports_file_path = DAILY_REPORTS_DIR_PATH + str(daily_reports_file_list[-2]).split('/')[-1].split(".")[
-            0] + '.csv'
+        daily_reports_file_path = DAILY_REPORTS_DIR_PATH +\
+            str(daily_reports_file_list[-2]).split('/')[-1].split(".")[
+                0] + '.csv'
         req = requests.get(daily_reports_file_path)
         url_content = req.content
         csv_file = open('daily_report.csv', 'wb')
@@ -52,23 +56,31 @@ class Command(BaseCommand):
                     country = Country(country=row[3])
                     country.save()
 
-                subdivision, _ = Subdivision.objects.get_or_create(country=country,
-                                                                   subdivision=row[2] or None,
-                                                                   fips=row[0] or None,
-                                                                   admin2=row[1] or None,
-                                                                   defaults={
-                                                                       'lat': row[5] or None,
-                                                                       'longitude': row[6] or None})
+                subdivision, _ = Subdivision.objects.\
+                    get_or_create(country=country,
+                                  subdivision=row[2] or None,
+                                  fips=row[0] or None,
+                                  admin2=row[1] or None,
+                                  defaults={
+                                    'lat': row[5] or None,
+                                    'longitude': row[6] or None
+                                  }
+                                  )
 
-                main, _ = MainTable.objects.update_or_create(country=country, subdivision=subdivision,
-                                                             defaults={'confirmed': row[7],
-                                                                       'deaths': row[8],
-                                                                       'recovered': row[9],
-                                                                       'active': row[10] or None,
-                                                                       'last_update': row[4],
-                                                                       'incidence_rate': row[12] or None,
-                                                                       'case_fatality_ratio': row[13] or None}
-                                                             )
+                main, _ = MainTable.\
+                    objects.update_or_create(
+                        country=country,
+                        subdivision=subdivision,
+                        defaults={'confirmed': row[7],
+                                  'deaths': row[8],
+                                  'recovered': row[9],
+                                  'active': row[10] or None,
+                                  'last_update': row[4],
+                                  'incidence_rate': row[12] or None,
+                                  # FIX: mainapp.models.MultipleObjectsReturned: get() returned more than one MainTable -- it returned 2!
+                                  'case_fatality_ratio': row[13] or None
+                                  }
+                            )
         print('MainTable fill done!')
 
         print('Getting TimeSeries data frame')
@@ -106,27 +118,27 @@ class Command(BaseCommand):
             df_result = pd.concat([df_result, df])
 
         # исправляем разные названия одной страны
-        df_result.loc[df_result['Country_Region'] == \
+        df_result.loc[df_result['Country_Region'] ==
                       'Mainland China', 'Country_Region'] = 'China'
-        df_result.loc[df_result['Country_Region'] == \
+        df_result.loc[df_result['Country_Region'] ==
                       ' Azerbaijan', 'Country_Region'] = 'Azerbaijan'
-        df_result.loc[df_result['Country_Region'] == \
+        df_result.loc[df_result['Country_Region'] ==
                       'Gambia, The', 'Country_Region'] = 'Gambia'
-        df_result.loc[df_result['Country_Region'] == \
+        df_result.loc[df_result['Country_Region'] ==
                       'Hong Kong SAR', 'Country_Region'] = 'China'
-        df_result.loc[df_result['Country_Region'] == \
+        df_result.loc[df_result['Country_Region'] ==
                       'Hong Kong', 'Country_Region'] = 'China'
-        df_result.loc[df_result['Country_Region'] == \
+        df_result.loc[df_result['Country_Region'] ==
                       'Iran (Islamic Republic of)', 'Country_Region'] = 'Iran'
-        df_result.loc[df_result['Country_Region'] == \
+        df_result.loc[df_result['Country_Region'] ==
                       'South Korea', 'Country_Region'] = 'Korea, South'
-        df_result.loc[df_result['Country_Region'] == \
+        df_result.loc[df_result['Country_Region'] ==
                       'Republic of Korea', 'Country_Region'] = 'Korea, South'
-        df_result.loc[df_result['Country_Region'] == \
+        df_result.loc[df_result['Country_Region'] ==
                       'Russian Federation', 'Country_Region'] = 'Russia'
-        df_result.loc[df_result['Country_Region'] == \
+        df_result.loc[df_result['Country_Region'] ==
                       'UK', 'Country_Region'] = 'United Kingdom'
-        df_result.loc[df_result['Country_Region'] == \
+        df_result.loc[df_result['Country_Region'] ==
                       'Taiwan', 'Country_Region'] = 'Taiwan*'
 
         # исправляем разные форматы дат
