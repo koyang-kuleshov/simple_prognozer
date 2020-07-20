ecc32058cd5ea25b9a809f35357b5eb9c35b64bc
sab1e <antony.sobolev@gmail.com>
2020-07-20 21:08:54 +0500

    refactor fill_TimeSeries

--- simple_prognozer/mainapp/management/commands/fill_TimeSeries.py
+++ simple_prognozer/mainapp/management/commands/fill_TimeSeries.py
@@ -1,3 +1,8 @@
+import csv
+import requests
+import codecs
+from contextlib import closing
+
 import pandas as pd
 from github.MainClass import Github
 
@@ -9,35 +14,97 @@ from mainapp.models import TimeSeries, Country, Subdivision
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        # очистка таблицы
+        TimeSeries.objects.all().delete()
         # настройки для подключения к github
-        token = ''
+        token = '6fc5c2794a42abca11763b27bf0a281454790001'
         repo_path = 'CSSEGISandData/COVID-19'
-        dr_repo_file_list = 'csse_covid_19_data/csse_covid_19_daily_reports'
+        dr_repo_file_list = 'csse_covid_19_data/csse_covid_19_time_series'
 
         # подключаемся
         git = Github(token)
         repo = git.get_repo(repo_path)
 
         # получаем список только отчетов
-        daily_reports_file_list = repo.get_contents(dr_repo_file_list)[1:-1]
+        time_series_file_list = repo.get_contents(dr_repo_file_list)[3:]
 
+        url = time_series_file_list[0].download_url
+
+        with closing(requests.get(url, stream=True)) as r:
+            reader = csv.reader(codecs.iterdecode(r.iter_lines(), 'utf-8'))
+            headers = next(reader)
+            # print(headers)
+            # for row in reader:
+            #
+            records = next(reader)
+            # создаем список объектов для записи в бд
+            model_instances = []
+            for num, record in enumerate(records[11:]):
+                print(Country.objects.get(
+                            country=records[7]))
+                print(records[6])
+                model_instance = TimeSeries(
+                    country=Country.objects.get(
+                        country=records[7]),
+                    subdivision=Subdivision.objects.get(
+                        country=Country.objects.get(
+                            country=records[7]),
+                        subdivision=records[6],
+                        fips=int(float(records[4])),
+                        admin2=records[5]
+                    ),
+                    last_update=headers[num],
+                    confirmed=record
+                )
+
+                # записываем данные в таблицу
+                TimeSeries.objects.bulk_create(model_instances)
+
+'''
         # создаем пустой фрейм для наполнения
-        df_result = pd.DataFrame(columns=['Last_Update',
-                                          'FIPS',
-                                          'Admin2',
-                                          'Province_State',
-                                          'Country_Region',
-                                          'Lat',
-                                          'Long_',
-                                          'Confirmed',
-                                          'Deaths',
-                                          'Recovered',
-                                          ])
+        df_time_series = pd.DataFrame(columns=['Last_Update',
+                                               'FIPS',
+                                               'Admin2',
+                                               'Province_State',
+                                               'Country_Region',
+                                               'Lat',
+                                               'Long_',
+                                               'Confirmed',
+                                               'Deaths',
+                                               'Recovered',
+                                               ])
 
+        df = pd.read_csv(time_series_file_list[0].download_url)
+
+        df = df.where(df.notnull(), None)
+        df_records = df.to_dict('records')
+
+        model_instances = [TimeSeries(
+            country=Country.objects.get_or_create(
+                country=record['Country_Region'])[0],
+            subdivision=Subdivision.objects.get_or_create(
+                country=Country.objects.get(
+                    country=record['Country_Region']),
+                subdivision=record['Province_State'],
+                fips=record['FIPS'],
+                admin2=record['Admin2']
+            )[0],
+            last_update=(record['Last_Update']),
+            confirmed=record['Confirmed'],
+            deaths=record['Deaths'],
+            recovered=record['Recovered'],
+        ) for record in df_records]
+
+        # записываем данные в таблицу
+        TimeSeries.objects.bulk_create(model_instances)
+
+
+       
         # перебираем отчеты по одному
-        for report in daily_reports_file_list:
+        for report in time_series_file_list:
             # загружаем отчет в датафрейм
             df = pd.read_csv(report.download_url)
+            print(df)
 
             # исправляем разночтения в названиях столбцов
             if {'Last Update'}.issubset(df.columns):
@@ -123,3 +190,4 @@ class Command(BaseCommand):
 
         print('TimeSeries fill done!')
 
+'''
